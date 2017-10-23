604a167d23e0d7e7fbfbbdc60cef2c01185270e9
Umar Niazi <uhkniazi@gmail.com>
2017-10-23 13:14:21 +0100

    first dataset downloaded and normalised

--- 01_downloadData_GSE7669.R
+++ 01_downloadData_GSE7669.R
@@ -11,6 +11,7 @@ library(downloader)
 ## open the soft format and raw data
 gse =  getGEO(filename = 'dataExternal/gse7669/GSE7669_series_matrix.txt.gz')
 
+## read raw data CEL files and normalize
 library(affy)
 setwd('dataExternal/gse7669/')
 untar('GSE7669_RAW.tar')
@@ -31,48 +32,55 @@ cn = gsub('^(GSM\\d+).CEL\\.gz', replacement = '\\1', cn, perl = T)
 table(cn %in% rownames(dfSamples))
 i = match(cn, rownames(dfSamples))
 dfSamples = dfSamples[i,]
+## sanity check
+identical(rownames(dfSamples), cn)
 
-## complete the expression set object
+## complete the expression set object by adding this sample information
 pData(x.affy) = dfSamples
 colnames(exprs(x.affy)) = cn
 # sanity check
-t1 = colnames(x.affy)
-t2 = rownames(pData(x.affy))
-table(t1 %in% t2)
-all(t1 == t2)
-## create grouping factors
-cvSource = as.character(pData(x.affy)[,'source_name_ch1'])
-table(cvSource)
-## create factors for cell type
-fCell.type = rep(NA, length=length(cvSource))
-i = grep('fibroblast', cvSource, ignore.case = T)
-fCell.type[i] = 'fibroblast'
-fCell.type[-i] = 'keratinocyte'
-fCell.type = factor(fCell.type)
-## keloid vs normal individual
-fDisorder = rep(NA, length=length(cvSource))
-i = grep('keloid', cvSource, ignore.case = T)
-fDisorder[i] = 'keloid'
-i = grep('without keloid', cvSource, ignore.case = T)
-fDisorder[i] = 'healthy'
-i = grep('non-lesion', cvSource, ignore.case = T)
-fDisorder[i] = 'keloid.non-lesion'
-# sanity check
-data.frame(fDisorder, cvSource)
-fDisorder = factor(fDisorder, levels = c('keloid', 'keloid.non-lesion', 'healthy'))
-table(fDisorder); levels(fDisorder)
-x.affy$fDisorder = fDisorder
-x.affy$fCell.type = fCell.type
+identical(colnames(x.affy), rownames(pData(x.affy)))
 
+# create database entries
+library('RMySQL')
 
+##### connect to mysql database to get samples
+db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
+dbListTables(db)
 
+# sample and metafile table
+dbGetQuery(db, paste('describe Sample;'))
+cSampleCol = dbGetQuery(db, paste('describe Sample;'))$Field[-1]
 
+dbGetQuery(db, paste('describe File;'))
+cFileCol = dbGetQuery(db, paste('describe MetaFile;'))$Field[-1]
 
+## create the entry for samples
+cSampleCol
+g_pid = 9
+g_did = 16
+title = paste(as.character(dfSamples$title), rownames(dfSamples))
+description = paste(as.character(dfSamples$source_name_ch1), as.character(dfSamples$characteristics_ch1))
+group1 = gsub('fibroblasts_(\\w+)_patient\\d+', '\\1', as.character(dfSamples$title))
+group2 = gsub('(female|male).+', '\\1', as.character(dfSamples$characteristics_ch1))
+group3 = gsub('\\D+', '', as.character(dfSamples$characteristics_ch1))
 
+dfSamples = data.frame(idProject=g_pid, idData=g_did, title=title, description=description, group1=group1, 
+                       group2=group2, group3=group3)
+# write this data to the database
+rownames(dfSamples) = NULL
 
+# # write this table to database
+# dbWriteTable(db, name='Sample', value=dfSamples, append=T, row.names=F)
 
+## annotation data
+fData(x.affy) = fData(gse)
+## store the grouping factors in affy object
+x.affy$fCondition = dfSamples$group1
+x.affy$fGender = dfSamples$group2
+x.affy$age = as.numeric(as.character(dfSamples$group3))
 
-
+## check normalisation 
 url = 'https://raw.githubusercontent.com/uhkniazi/CDiagnosticPlots/master/CDiagnosticPlots.R'
 download(url, 'CDiagnosticPlots.R')
 
@@ -81,13 +89,15 @@ source('CDiagnosticPlots.R')
 # delete the file after source
 unlink('CDiagnosticPlots.R')
 
-oDiag.1 = CDiagnosticPlots(d, 'method 1')
+oDiag.1 = CDiagnosticPlots(log(exprs(gse)+1), 'Soft format')
+oDiag.2 = CDiagnosticPlots(exprs(x.affy), 'Raw normalised')
 
-fBatch = as.character(oExp$title)
-fBatch = factor(gsub('fibroblasts_(\\w+)_patie.+', '\\1', fBatch))
+fBatch = x.affy$fCondition
 
 ## compare the 2 methods using various plots
+par(mfrow=c(1,2))
 boxplot.median.summary(oDiag.1, fBatch, legend.pos = 'topright', axis.label.cex = 0.7)
+boxplot.median.summary(oDiag.2, fBatch, legend.pos = 'topright', axis.label.cex = 0.7)
 
 plot.mean.summary(oDiag.1, fBatch, axis.label.cex = 0.7)
 plot.mean.summary(oDiag.2, fBatch, axis.label.cex = 0.7)
@@ -104,23 +114,6 @@ plot.PCA(oDiag.2, fBatch, cex.main=1)
 plot.dendogram(oDiag.1, fBatch, labels_cex = 0.8, cex.main=0.7)
 plot.dendogram(oDiag.2, fBatch, labels_cex = 0.8, cex.main=0.7)
 
-## how about the extreme values
-oDiag.1 = calculateExtremeValues(oDiag.1)
-oDiag.2 = calculateExtremeValues(oDiag.2)
-m1 = mGetExtremeValues(oDiag.1)
-m2 = mGetExtremeValues(oDiag.2)
-
-## samples with most extreme values
-apply(m1, 2, function(x) sum(x > 0))
-apply(m2, 2, function(x) sum(x > 0))
-
-## variables that are contributing to this
-v1 = apply(m1, 1, function(x) sum(x > 0))
-v2 = apply(m2, 1, function(x) sum(x > 0))
-
-which(v1 > 0)
-which(v2 > 0)
-
 ## change parameters 
 l = CDiagnosticPlotsGetParameters(oDiag.1)
 l$PCA.jitter = F
@@ -129,14 +122,26 @@ l$HC.jitter = F
 ## this should give an error as scaling can't be done
 ## if all the vector 0 for PCA
 oDiag.1.2 = CDiagnosticPlotsSetParameters(oDiag.1, l)
-## reset flag for jittering
-l$PCA.jitter = T
-## should work this time
-oDiag.1.2 = CDiagnosticPlotsSetParameters(oDiag.1, l)
-plot.PCA(oDiag.1, fBatch, legend.pos = 'topright')
-plot.PCA(oDiag.1.2, fBatch)
-plot.dendogram(oDiag.1, fBatch)
-plot.dendogram(oDiag.1.2, fBatch)
+oDiag.2.2 = CDiagnosticPlotsSetParameters(oDiag.2, l)
 
+plot.PCA(oDiag.1.2, fBatch, legend.pos = 'topright')
+plot.PCA(oDiag.2.2, fBatch, legend.pos = 'topright')
 
-##############################################################################
+plot.dendogram(oDiag.1.2, fBatch, labels_cex = 0.8, cex.main=0.7)
+plot.dendogram(oDiag.2.2, fBatch, labels_cex = 0.8, cex.main=0.7)
+
+### save the x.affy object and create metafile entry
+getwd()
+n = make.names(paste('GSE7669 Synovial fibroblasts, RA versus OA.rds'))
+n2 = paste0('~/Data/MetaData/', n)
+save(x.affy, file=n2)
+
+## access the mysql database to create entry for this data object
+library('RMySQL')
+db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
+dbListTables(db)
+dbListFields(db, 'MetaFile')
+df = data.frame(idData=g_did, name=n, type='rds', location='~/Data/MetaData/', comment='GSE7669 Synovial fibroblasts, RA versus OA, normalised object')
+dbWriteTable(db, name = 'MetaFile', value=df, append=T, row.names=F)
+dbDisconnect(db)
+
